1487a09b5ba39caa3735f65aa2c8a7b073218913
Jose Manuel Montero <jmmontero.ortega@gmail.com>
2019-03-16 17:52:35 +0100

    Initial commit and add models.

---
+++ LinqWorkShop/Model/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWorkShop
+{
+    public class Actor
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public SexEnum Sex { get; set; }
+    }
+}
---
+++ LinqWorkShop/Model/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqWorkShop
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public GenreEnum Genre { get; set; }
+
+        public List<Actor> Actors { get; set; } = new List<Actor>();
+    }
+}
